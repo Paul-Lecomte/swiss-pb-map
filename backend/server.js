da8e8492c76638e8f1317cdb0ce246cd33b5ce23
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-02 13:07:59 +0100

    server.js

---
+++ backend/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const { errorHandler } = require('./middleware/errorHandler');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const connectDB = require('./config/dbConnection');
+const cookieParser = require('cookie-parser');
+const http = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+// Connect to the database
+connectDB();
+
+// Server config
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+// Routes
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the RailQuest API!');
+});
+
+// Error handling middleware
+app.use(errorHandler);
+
+// Create an HTTP server for WebSockets
+const server = http.createServer(app);
+
+// Start the server after connecting to MongoDB
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    server.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    });
+});
+
+// Handle MongoDB connection errors
+mongoose.connection.on('error', (err) => {
+    console.log(`MongoDB connection error: ${err}`);
+});
