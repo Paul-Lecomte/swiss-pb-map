17f30d7badd000b29c4ba09c69fc5242e0e7598f
Paul <lecomtepaulvd@gmail.com>
2025-03-04 12:36:47 +0100

    finished the get all stops controller

--- backend/controller/stopController.js
+++ backend/controller/stopController.js
@@ -2,8 +2,12 @@ const asyncHandler = require('express-async-handler');
 const Stops = require("../model/stopsModel")
 
 const getAllStops = asyncHandler(async (req, res) => {
-    const stops = [];
-
+    try {
+        const stops = await Stops.find({});
+        res.status(200).json(stops);
+    } catch(error) {
+        res.status(500).json({ message: 'Failed to fetch stops', error: error.message });
+    }
 })
 
 module.exports = {
