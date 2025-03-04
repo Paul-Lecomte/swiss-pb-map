5af35320475b78d9a91d380ebb68d79f17953922
Paul <lecomtepaulvd@gmail.com>
2025-03-04 12:53:56 +0100

    base for getting the trip with the stop_id

--- backend/controller/tripController.js
+++ backend/controller/tripController.js
@@ -1,3 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const Stoptime = require("../model/stopTimesModel");
 const Trip = require("../model/tripsModel");
+const {getAllStops} = require("./stopController");
+
+const getTrip = asyncHandler(async (req, res) => {
+
+})
+
+module.exports = {
+    getTrip,
+}
