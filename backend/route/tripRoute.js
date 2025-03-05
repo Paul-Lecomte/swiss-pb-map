e5f6eb3e3438bc48f0c6c38bb7e44c1f4e5b8f6d
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-05 09:25:27 +0100

    timetabble route added

--- backend/route/tripRoute.js
+++ backend/route/tripRoute.js
@@ -7,4 +7,9 @@ const router = express.Router();
 // @access   public
 router.route('/:stop_id').get(tripController.getTrip);
 
+// @desc     Get timetable for a given stop
+// @route    GET /api/timetable/:_id
+// @access   public
+router.route('/timetable/:stop_id').get(tripController.getTimetable);
+
 module.exports = router;
