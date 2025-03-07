7d28d39d1457c78b277856f2c11ee430d7cf2376
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-07 09:58:53 +0100

    added the route

--- backend/route/tripRoute.js
+++ backend/route/tripRoute.js
@@ -8,8 +8,13 @@ const router = express.Router();
 router.route('/:stop_id').get(tripController.getTrip);
 
 // @desc     Get timetable for a given stop
-// @route    GET /api/timetable/:_id
+// @route    GET /api/trip/timetable/:_id
 // @access   public
 router.route('/timetable/:stop_id').get(tripController.getTimetable);
+
+// @desc     Get all the stops
+// @route    GET /api/trip/all
+// @access   public
+router.route('/all').get(tripController.getAllStops);
 
 module.exports = router;
