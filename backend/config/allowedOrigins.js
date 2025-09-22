dba4d930e5ced82a4374b34b29ae74e07f2d66b2
Paul <lecomtepaulvd@gmail.com>
2025-09-22 10:42:09 +0200

    cors allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const {all} = require("express/lib/application");
 const allowedOrigins = [
     'http://localhost:3000',
+    'http://localhost:3001',
     'http://localhost:5173',
     'http://localhost:5174'
 ]
