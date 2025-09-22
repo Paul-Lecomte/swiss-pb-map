c088a99bffa8263d50da41ed09e2c709a45a8bb5
Paul <lecomtepaulvd@gmail.com>
2025-09-22 10:42:43 +0200

    working call to backend to fetch all processed stops

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,31 +1,49 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import ZoomControl from "../zoom/ZoomControl";
 import "leaflet/dist/leaflet.css";
+import { fetchProcessedStops } from "../../services/StopsApiCalls";
 
-const Map = () => (
-    <div
-        style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width: "100vw",
-            height: "100vh",
-            zIndex: 0,
-        }}
-    >
-        <MapContainer
-            center={[46.516, 6.63282]}
-            zoom={13}
-            zoomControl={false}
-            style={{ position: "relative", width: "100%", height: "100%" }}
+const Map = () => {
+    const [stops, setStops] = useState([]);
+
+    useEffect(() => {
+        const getStops = async () => {
+            try {
+                const data = await fetchProcessedStops();
+                setStops(data);
+            } catch (error) {
+                console.error("Erreur lors du fetch des arrêts:", error);
+            }
+        };
+        getStops();
+    }, []);
+
+    return (
+        <div
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100vw",
+                height: "100vh",
+                zIndex: 0,
+            }}
         >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <ZoomControl />
-        </MapContainer>
-    </div>
-);
+            <MapContainer
+                center={[46.516, 6.63282]}
+                zoom={13}
+                zoomControl={false}
+                style={{ position: "relative", width: "100%", height: "100%" }}
+            >
+                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <ZoomControl />
+                {/* Custom stop marker here */}
+            </MapContainer>
+        </div>
+    );
+};
 
 export default Map;
