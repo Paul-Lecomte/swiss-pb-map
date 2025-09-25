83f9cd7e8bd37bd8865eaed6b3ff9275ffec5068
Paul <lecomtepaulvd@gmail.com>
2025-09-25 09:01:06 +0200

    inversed lat long for correct placement of the stops

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,8 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import ZoomControl from "../zoom/ZoomControl";
 import { fetchStopsInBbox } from "../../services/StopsApiCalls";
+
+const busIcon = '/icons/bus_marker.png';
+const metroIcon = '/icons/metro_marker.png';
+const trainIcon = '/icons/train_marker.png';
+
+const createLeafletIcon = (iconUrl: string) =>
+    new L.Icon({
+        iconUrl,
+        iconSize: [24, 24],
+        iconAnchor: [16, 32],
+        popupAnchor: [0, -32],
+    });
 
 const Map = () => {
     const [stops, setStops] = useState([]);
@@ -13,7 +26,7 @@ const Map = () => {
             const data = await fetchStopsInBbox(bbox, zoom);
             setStops(data.features || []);
         } else {
-            setStops([]); // Vide la liste si pas au zoom max
+            setStops([]);
         }
     };
 
@@ -52,11 +65,24 @@ const Map = () => {
     }
 
     useEffect(() => {
-        // bbox initiale
         const bbox = [6.5, 46.5, 6.7, 46.6];
-        // 18 est souvent le zoom max sur Leaflet, adapte si besoin
         loadStops(bbox, 13, 18);
     }, []);
+
+    function getStopIcon(routeDesc: string) {
+        switch (routeDesc) {
+            case 'M':
+                return createLeafletIcon(metroIcon);
+            case 'B':
+                return createLeafletIcon(busIcon);
+            case 'EV':
+            case 'S':
+            case 'SN':
+                return createLeafletIcon(trainIcon);
+            default:
+                return createLeafletIcon(busIcon);
+        }
+    }
 
     return (
         <div style={{ position: "fixed", top: 0, left: 0, width: "100vw", height: "100vh", zIndex: 0 }}>
@@ -67,11 +93,22 @@ const Map = () => {
                 zoomControl={false}
                 style={{ position: "relative", width: "100%", height: "100%" }}
             >
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                <TileLayer
+                    url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+                    subdomains={["a", "b", "c", "d"]}
+                />
                 <ZoomControl />
                 <MapEvents />
                 {stops.map((stop: any, idx: number) => (
-                    <Marker key={idx} position={stop.geometry.coordinates}>
+                    <Marker
+                        key={idx}
+                        position={[
+                            stop.geometry.coordinates[1], // lat
+                            stop.geometry.coordinates[0], // lng
+                        ]}
+                        icon={getStopIcon(stop.properties.routeDesc)}
+                    >
                         <Popup>{stop.properties.name}</Popup>
                     </Marker>
                 ))}
