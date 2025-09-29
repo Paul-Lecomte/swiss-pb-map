f04fcc8ae905da10d84a0d529778f0a1f247ae55
Paul <lecomtepaulvd@gmail.com>
2025-09-29 07:05:56 +0200

    next big step

---
+++ frontend/src/components/vehicle_position/VehiclePosition.tsx
@@ -0,0 +1,7 @@
+// This file is where we will have the logic and styling for the vehicle position component
+// It will display the vehicle's current position on the map and update as the vehicle moves
+// the position will b e determined by the time berween the last stop and the next stop
+// and the time the vehicle is expected to arrive at the next stop
+// we will use a simple linear interpolation to determine the position
+// we will also display the vehicle's heading and speed if available
+// we will use a simple arrow to indicate the heading
